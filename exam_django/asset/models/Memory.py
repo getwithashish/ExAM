f14502e22e24edb7bafc14b2cb84b67c69e376e8
Ashi941 <asimapal09@gmail.com>
2024-02-19 12:31:54 +0530

    feat - Added a blank attribute set to false in memory_space

--- exam_django/asset/models/Memory.py
+++ exam_django/asset/models/Memory.py
@@ -3,7 +3,7 @@ import uuid
 
 class Memory(models.Model):
   memory_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-  memory_space = models.IntegerField(editable=False,default = 0,null= False)
+  memory_space = models.IntegerField(editable=False,default = 0,null= False,blank=False)
 
   def __str__(self):
     return str(self.memory_space)
