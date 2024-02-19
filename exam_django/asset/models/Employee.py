1c8da50f7147c4dceaa857ebb59ae1aec0947f3f
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-19 12:34:59 +0530

    feat: Add Employee Model

---
+++ exam_django/asset/models/Employee.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Employee(models.Model):
+    employee_id=models.UUIDField(primary_key=True,editable=False)
+    employee_name=models.CharField(max_length=255,null=False,blank=False)
+    employee_department=models.CharField(max_length=255,null=False,blank=False)
+    employee_designation=models.CharField(max_length=255,null=False,blank=False)
+
