4630ee89583418fc0ace2c409e585ec4a2afe31c
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-20 10:39:28 +0530

    feat: Retrieve list of business units (#5)
    * feat: BusinessUnit
    Post
    
    * feat: Retrieve businessunit details
    
    * fix:Retrieve BusinessUnit
    
    * fix: Change bu to businessunit in BusinessUnitModel

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import AssetTypeView
+from django.urls import path, include
+from asset.views import AssetTypeView, BusinessUnitView
 
 urlpatterns = [
-    path('asset_type', AssetTypeView.as_view(), name = 'asset-type')
+    path('asset_type', AssetTypeView.as_view(), name = 'asset-type'),
+    path('businessunit/', BusinessUnitView.as_view(), name='businessunit')
 ]
