6ba347c94fecd3802fd77c273ccca53800be9837
AnanthanCJ <152945570+AnanthanCJ@users.noreply.github.com>
2024-02-20 14:23:26 +0530

    feat: Retrieve asset count based on asset status (#10)
    * feat:Add location serializer
    
    * feat: retieve asset count on the basis of status
    
    ---------
    
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>

--- exam_django/asset/views/__init__.py
+++ exam_django/asset/views/__init__.py
@@ -1,10 +1,12 @@
 from .AssetTypeView import AssetTypeView
 from .BusinessUnitView import BusinessUnitView
 from .LocationView import LocationView
+from .AssetCountView import AssetCountView
 
 
 __all__ = [
     'AssetTypeView',
     'BusinessUnitView',
-    'LocationView'
+    'LocationView',
+    'AssetCountView'
     ]
