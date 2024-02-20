c7a72ca10d28fb0618276ed64dfbcac991b5ffa8
Ashi941 <asimapal09@gmail.com>
2024-02-20 18:11:46 +0530

    fix: delete SearchAssetNamesSerializer

--- exam_django/asset/serializers/SearchAssetNamesSerializer.py
+++
@@ -1,8 +0,0 @@
-from asset.models import Asset
-from rest_framework import serializers
-
-
-class SearchAssetnamesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Asset
-        fields = "__all__"
