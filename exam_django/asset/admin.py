265c43b0b890b83e3967cfe921936bea3675eca3
Aidrin Varghese <aidrinv2001@gmail.com>
2024-02-20 18:17:38 +0530

    feat: Add Asset views & test: Asset, AssetTypes & User

--- exam_django/asset/admin.py
+++ exam_django/asset/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from asset.models import AssetType, User
+from asset.models import AssetType, User, Asset
 
 # Register your models here.
 admin.site.register(AssetType)
 admin.site.register(User)
+admin.site.register(Asset)
