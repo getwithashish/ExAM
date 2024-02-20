c6eae691466717654722c04c5325b83ee6559234
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-20 09:35:47 +0530

    fix:Employee url

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -8,12 +8,4 @@ urlpatterns=[
 
     path('employee/',EmployeeView.as_view(),name='employee')
 ]
-from django.urls import include, path
-from asset.views import BusinessUnitView,EmployeeView
-from .views import *
 
-
-urlpatterns=[
-
-    path('businessunit/', BusinessUnitView.as_view(),name='businessunit')
-]
