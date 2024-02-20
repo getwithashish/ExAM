371771a053fcb18187fe0ee36748b4352c6feeda
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-20 17:25:01 +0530

    fix: Add employee endpoint to url

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('location', LocationView.as_view(), name = "location"),
     path('asset_count',AssetCountView.as_view(),name= "asset_count"),
     path('conceder', ConcederView.as_view(), name='conceder-list'),
-     path('asset_type', AssetTypeView.as_view(), name = 'asset-type'),
     path('businessunit/', BusinessUnitView.as_view(), name='businessunit'),
+
 
 ]
 
