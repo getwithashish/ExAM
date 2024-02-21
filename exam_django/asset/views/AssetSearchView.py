6b8a23832b4ab66cd82ae31c7b4dbb396d87e272
Ashi941 <asimapal09@gmail.com>
2024-02-21 11:58:49 +0530

    Add AssetSearchView and deleted assetSearchBySerialNumberView

--- exam_django/asset/views/AssetSearchView.py
+++ exam_django/asset/views/AssetSearchView.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.views import View
 from asset.models import Asset
+from asset.serializers import AssetSerializer
+from rest_framework.generics import ListAPIView
 
 # Search assets by name
 class AssetSearchByNameView(View):
@@ -13,3 +15,16 @@ class AssetSearchByNameView(View):
         
         data = list(assets.values())
         return JsonResponse(data, safe=False)
+
+# Search assets by serial number 
+class AssetSearchBySerialNumberAPIView(ListAPIView):
+    serializer_class = AssetSerializer
+
+    def get_queryset(self):
+        serial_number = self.request.query_params.get('serial_number', None)
+        if serial_number:
+            # Filtering assets based on serial number starting with the provided value
+            queryset = Asset.objects.filter(serial_number__startswith=serial_number)
+            return queryset
+        else:
+            return Asset.objects.none()  # Return an empty queryset if no serial number provided
