cfb61a30297de3a027006c4be9cc1ee1f35b56a6
Asima Pal <91839451+Ashi941@users.noreply.github.com>
2024-02-21 13:08:01 +0530

    feat: Retrieve and add memory spaces values (#6)
    * feat: Retrieve and add memory values
    
    * Add correct naming conventions
    
    * fix: Add .vscode to .gitignore
    
    ---------
    
    Co-authored-by: TC4Y <techarmy257@gmail.com>

--- exam_django/asset/serializers/__init__.py
+++ exam_django/asset/serializers/__init__.py
@@ -3,6 +3,7 @@ from .AssetSerializer import AssetSerializer
 from .UserSerializer import UserSerializer
 from .AssetTypeSerializer import AssetTypeSerializer
 from .BusinessUnitSerializer import BusinessUnitSerializer
+from .MemorySerializer import MemorySerializer
 from .EmployeeSerializer import EmployeeSerializer
 __all__ = [
     'LocationSerializer',
@@ -10,5 +11,6 @@ __all__ = [
     'UserSerializer',
     'AssetTypeSerializer',
     'BusinessUnitSerializer',
+    'MemorySerializer',
     'EmployeeSerializer'
 ]
