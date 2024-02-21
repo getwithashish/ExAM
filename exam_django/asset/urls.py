d997bb5f981f05b6eaca1279f3f54465848ae64b
Ashi941 <asimapal09@gmail.com>
2024-02-21 05:58:49 +0530

    feat: Search asset by model numbers

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -1,15 +1,16 @@
 #exam_django/asset/urls.py
 
 from django.urls import path
-from .views import LocationView,AssetCountView,ConcederView, AssetTypeView, BusinessUnitView
+from .views import LocationView,AssetCountView,ConcederView, AssetTypeView, BusinessUnitView,AssetSearchByModelNumberView
 
 urlpatterns = [
     
     path('location', LocationView.as_view(), name = "location"),
     path('asset_count',AssetCountView.as_view(),name= "asset_count"),
     path('conceder', ConcederView.as_view(), name='conceder-list'),
-     path('asset_type', AssetTypeView.as_view(), name = 'asset-type'),
+    path('asset_type', AssetTypeView.as_view(), name = 'asset-type'),
     path('businessunit/', BusinessUnitView.as_view(), name='businessunit'),
+    path('searchbymodelnumber/', AssetSearchByModelNumberView.as_view(), name='asset_search'),
 
 ]
 
