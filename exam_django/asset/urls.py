5e39cebe192a66f5be3682af2e2c79056f09d71b
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-22 00:28:41 +0530

    feat:Update enpoint url

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -42,5 +42,5 @@ urlpatterns = [
     ),
     path("memory_list/", MemoryView.as_view(), name="memory_list"),
     path("employee/", EmployeeView.as_view(), name="employeeview"),
-    path("assignasset/", AssignAssetView.as_view(), name="assignasset"),
+    path("assign_asset/", AssignAssetView.as_view(), name="assignasset"),
 ]
