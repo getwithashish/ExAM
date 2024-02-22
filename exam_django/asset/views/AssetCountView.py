37bf68ca925916cb5b5ca3b728dafc8ac20fae7c
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-02-22 16:17:17 +0530

    fix: Limit functionality of AssetCountView to only fetch count of assets

--- exam_django/asset/views/AssetCountView.py
+++ exam_django/asset/views/AssetCountView.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
 from asset.models import Asset
 from django.db.models import Count
 
 
-class AssetCountView(ListCreateAPIView):
+class AssetCountView(ListAPIView):
     def list(self, request, *args, **kwargs):
         # Get a dictionary with status counts
         status_counts = Asset.objects.values("status").annotate(count=Count("status"))
