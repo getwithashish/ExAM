83f03bf706cbeddc4fa20b36c3128f1ae45bcade
AnanthanCJ <acj88177@gmail.com>
2024-02-25 14:15:00 +0530

    fix: Retrives only approved count status with softwares and hardwares showing separately

--- exam_django/asset/views/AssetCountView.py
+++ exam_django/asset/views/AssetCountView.py
@@ -1,19 +1,27 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
-
 from asset.models import Asset
 from django.db.models import Count
 
 
 class AssetCountView(ListCreateAPIView):
     def list(self, request, *args, **kwargs):
-        # Get a dictionary with status counts
-        status_counts = Asset.objects.values("status").annotate(count=Count("status"))
+        # Get counts for hardware assets
+        hardware_counts = Asset.objects.filter(asset_category="HARDWARE", approval_status="APPROVED").values("status").annotate(count=Count("status"))
+        
+        # Get counts for software assets
+        software_counts = Asset.objects.filter(asset_category="SOFTWARE", approval_status="APPROVED").values("status").annotate(count=Count("status"))
 
-        # Create a response dictionary with status counts
-        response_data = [
-            {"status": item["status"], "count": item["count"]} for item in status_counts
-        ]
+        # Create a response dictionary with counts for hardware and software separately
+        response_data = {
+            "hardware": [
+                {"status": item["status"], "count": item["count"]} for item in hardware_counts
+            ],
+            "software": [
+                {"status": item["status"], "count": item["count"]} for item in software_counts
+            ]
+        }
 
         # Return the response
         return Response(response_data)
+
