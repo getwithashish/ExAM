4380f34c0ec7aa47e71c04babcb5e5ee3ead393c
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-26 22:02:15 +0530

    feat:Add try exception block

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -3,7 +3,7 @@ from asset.views import AssetTypeView, BusinessUnitView, LocationView, AssetCoun
 
 urlpatterns = [
     path('asset_type', AssetTypeView.as_view(), name = 'asset-type'),
-    path('businessunit/', BusinessUnitView.as_view(), name='businessunit'),
+    path('business_unit/', BusinessUnitView.as_view(), name='businessunit'),
     path('location', LocationView.as_view(), name = "location"),
     path('asset_count',AssetCountView.as_view(),name= "asset_count")
 ]
