01bdbf9c7837f16ac84e6a32b6cc80b08e042a65
Ashish Sam T George <77051732+getwithashish@users.noreply.github.com>
2024-02-26 07:47:25 +0530

    feat: Retreive and update individual asset details (#21)
    * fix: Limit functionality of AssetCountView to only fetch count of assets
    
    * fix: Format code using black formatter
    
    * feat: Retrieve and update details of individual assets

--- exam_django/asset/models/Asset.py
+++ exam_django/asset/models/Asset.py
@@ -37,7 +37,9 @@ class Asset(models.Model):
     asset_id = models.CharField(max_length=255, null=True, blank=False)
     version = models.IntegerField(default=0)
     asset_category = models.CharField(max_length=50, choices=asset_category_choices)
-    asset_type = models.ForeignKey("AssetType", on_delete=models.CASCADE, default="employee")
+    asset_type = models.ForeignKey(
+        "AssetType", on_delete=models.CASCADE, default="employee"
+    )
     product_name = models.CharField(max_length=255, null=False)
     model_number = models.CharField(
         max_length=255, null=True, blank=False, default=None
@@ -45,8 +47,11 @@ class Asset(models.Model):
     serial_number = models.IntegerField(null=True, blank=False, default=None)
     owner = models.CharField(max_length=50, choices=owner_choices)
     custodian = models.ForeignKey(
-        "Employee", related_name="assets_custodian", on_delete=models.CASCADE, default="employee"
-     )
+        "Employee",
+        related_name="assets_custodian",
+        on_delete=models.CASCADE,
+        default="employee",
+    )
     date_of_purchase = models.DateField(null=False)
     status = models.CharField(max_length=50, default="IN STORE", choices=status_choices)
     warranty_period = models.IntegerField(null=False)
@@ -57,8 +62,10 @@ class Asset(models.Model):
         "Location", related_name="assets_invoice_location", on_delete=models.CASCADE
     )
     business_unit = models.ForeignKey(
-        "BusinessUnit", on_delete=models.CASCADE, null=False,
-     )
+        "BusinessUnit",
+        on_delete=models.CASCADE,
+        null=False,
+    )
     os = models.CharField(max_length=50, null=True, blank=True, choices=os_choices)
     os_version = models.CharField(max_length=50)
     mobile_os = models.CharField(max_length=100, null=True, blank=True)
@@ -69,10 +76,12 @@ class Asset(models.Model):
     configuration = models.TextField()
     accessories = models.CharField(max_length=50, null=True, blank=True)
     notes = models.CharField(max_length=255)
-    conceder = models.ForeignKey('User', on_delete=models.CASCADE)
+    conceder = models.ForeignKey("User", on_delete=models.CASCADE)
     approval_status = models.CharField(
         max_length=50, default="PENDING", choices=approval_status_choices
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+    def __str__(self):
+        return str(self.product_name)
