ca7d100437a82b22d658bc2a31a60bcce8d475a0
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-02-27 12:42:43 +0530

    fix: Add constraints to Asset model fields

--- exam_django/asset/models/Asset.py
+++ exam_django/asset/models/Asset.py
@@ -34,49 +34,65 @@ class Asset(models.Model):
     asset_id = models.CharField(max_length=255, null=True, blank=False)
     version = models.IntegerField(default=0)
     asset_category = models.CharField(max_length=50, choices=asset_category_choices)
-    asset_type = models.ForeignKey(
-        "AssetType",
-        on_delete=models.CASCADE
-    )
+    asset_type = models.ForeignKey("AssetType", on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255, null=False)
     model_number = models.CharField(
         max_length=255, null=True, blank=False, default=None
     )
-    serial_number = models.IntegerField(null=True, blank=False, default=None)
-    owner = models.CharField(max_length=50, choices=owner_choices)
+    serial_number = models.CharField(
+        max_length=255, null=True, blank=False, default=None
+    )
+    owner = models.CharField(max_length=50, choices=owner_choices, null=False)
     custodian = models.ForeignKey(
-        "Employee",
-        related_name="assets_custodian",
-        on_delete=models.CASCADE
+        "Employee", related_name="assets_custodian", on_delete=models.CASCADE, null=True
     )
     date_of_purchase = models.DateField(null=False)
-    status = models.CharField(max_length=50, default="IN STORE", choices=status_choices)
-    status = models.CharField(max_length=50, default="IN STORE", choices=status_choices)
-    warranty_period = models.IntegerField(null=False)
+    status = models.CharField(
+        max_length=50,
+        default="IN STORE",
+        choices=status_choices,
+        null=False,
+        blank=False,
+    )
+    warranty_period = models.IntegerField(null=True, blank=False)
     location = models.ForeignKey(
-        "Location", related_name="assets_location", on_delete=models.CASCADE
+        "Location",
+        related_name="assets_location",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=False,
     )
     invoice_location = models.ForeignKey(
-        "Location", related_name="assets_invoice_location", on_delete=models.CASCADE
+        "Location",
+        related_name="assets_invoice_location",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=False,
     )
     business_unit = models.ForeignKey(
         "BusinessUnit",
         on_delete=models.CASCADE,
         null=False,
     )
-    os = models.CharField(max_length=50, null=True, blank=True, choices=os_choices)
-    os_version = models.CharField(max_length=50)
-    mobile_os = models.CharField(max_length=100, null=True, blank=True)
-    processor = models.CharField(max_length=100, null=True, blank=True)
-    processor_gen = models.CharField(max_length=100, null=True, blank=True)
-    memory = models.CharField(max_length=50, null=True, blank=True)
-    storage = models.CharField(max_length=50, null=True, blank=True)
-    configuration = models.TextField()
-    accessories = models.CharField(max_length=50, null=True, blank=True)
+    os = models.CharField(max_length=50, null=True, blank=False, choices=os_choices)
+    os_version = models.CharField(max_length=50, null=True, blank=False)
+    mobile_os = models.CharField(max_length=100, null=True, blank=False)
+    processor = models.CharField(max_length=100, null=True, blank=False)
+    processor_gen = models.CharField(max_length=100, null=True, blank=False)
+    memory = models.ForeignKey("Memory", on_delete=models.CASCADE, null=True)
+    storage = models.CharField(max_length=50, null=True, blank=False)
+    configuration = models.TextField(null=True, blank=False)
+    accessories = models.CharField(max_length=50, null=True, blank=False)
     notes = models.CharField(max_length=255)
-    conceder = models.ForeignKey("User", on_delete=models.CASCADE)
+    conceder = models.ForeignKey(
+        "User", on_delete=models.CASCADE, null=True, blank=False
+    )
     approval_status = models.CharField(
-        max_length=50, default="PENDING", choices=approval_status_choices
+        max_length=50,
+        default="PENDING",
+        choices=approval_status_choices,
+        null=False,
+        blank=False,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
