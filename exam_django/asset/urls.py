ee2e09ef0aa6b283926421c8534f4cad674f0d84
Asima Pal <91839451+Ashi941@users.noreply.github.com>
2024-02-29 01:03:04 +0530

    Feat asset search functionalities (#41)
    * feat: Add various Asset search functionalities
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * fix: Search api
    
    * feat: Add search with filter function
    
    * feat: Add Asset Search functionality based on different fields
    
    ---------
    
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -6,14 +6,10 @@ from asset.views import (
     ConcederView,
     AssetTypeView,
     BusinessUnitView,
-    AssetSearchByNameView,
-    AssetSearchBySerialNumberAPIView,
-    AssetSearchByModelNumberView,
-    AssetSearchByAssetIDView,
     MemoryView,
     EmployeeView,
     AssignAssetView,
-    TriggerView,
+    AssetSearchWithFilterView,
 )
 
 
@@ -24,25 +20,8 @@ urlpatterns = [
     path("conceder", ConcederView.as_view(), name="conceder-list"),
     path("asset_type", AssetTypeView.as_view(), name="asset-type"),
     path("business_unit", BusinessUnitView.as_view(), name="businessunit"),
-    path(
-        "search_by_name", AssetSearchByNameView.as_view(), name="search_asset_by_name"
-    ),
-    path(
-        "search_by_serial_number",
-        AssetSearchBySerialNumberAPIView.as_view(),
-        name="search_by_serial_number",
-    ),
-    path(
-        "search_by_model_number",
-        AssetSearchByModelNumberView.as_view(),
-        name="asset_search",
-    ),
-    path(
-        "search_by_id",
-        AssetSearchByAssetIDView.as_view(),
-        name="asset_search_by_asset_id",
-    ),
     path("memory_list", MemoryView.as_view(), name="memory_list"),
     path("employee", EmployeeView.as_view(), name="employeeview"),
     path("assign_asset", AssignAssetView.as_view(), name="assignasset"),
+    path("search", AssetSearchWithFilterView.as_view(), name="search"),
 ]
