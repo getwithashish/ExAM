71f9a29d4e929bff3b6a6e15d2c61090f37414a4
AnanthanCJ <acj88177@gmail.com>
2024-02-28 17:46:45 +0530

    fix: resolved duplication issue in asset log json field

--- exam_django/asset/urls.py
+++ exam_django/asset/urls.py
@@ -22,26 +22,26 @@ urlpatterns = [
     path("asset_count", AssetCountView.as_view(), name="asset_count"),
     path("conceder", ConcederView.as_view(), name="conceder-list"),
     path("asset_type", AssetTypeView.as_view(), name="asset-type"),
-    path("business_unit/", BusinessUnitView.as_view(), name="businessunit"),
+    path("business_unit", BusinessUnitView.as_view(), name="businessunit"),
     path(
-        "search_by_name/", AssetSearchByNameView.as_view(), name="search_asset_by_name"
+        "search_by_name", AssetSearchByNameView.as_view(), name="search_asset_by_name"
     ),
     path(
-        "search_by_serial_number/",
+        "search_by_serial_number",
         AssetSearchBySerialNumberAPIView.as_view(),
         name="search_by_serial_number",
     ),
     path(
-        "search_by_model_number/",
+        "search_by_model_number",
         AssetSearchByModelNumberView.as_view(),
         name="asset_search",
     ),
     path(
-        "search_by_id/",
+        "search_by_id",
         AssetSearchByAssetIDView.as_view(),
         name="asset_search_by_asset_id",
     ),
-    path("memory_list/", MemoryView.as_view(), name="memory_list"),
-    path("employee/", EmployeeView.as_view(), name="employeeview"),
-    path("assign_asset/", AssignAssetView.as_view(), name="assignasset"),
+    path("memory_list", MemoryView.as_view(), name="memory_list"),
+    path("employee", EmployeeView.as_view(), name="employeeview"),
+    path("assign_asset", AssignAssetView.as_view(), name="assignasset"),
 ]
--- exam_django/asset/views/AssetView.py
+++ exam_django/asset/views/AssetView.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from asset.serializers import AssetReadSerializer, AssetSerializer
+from asset.serializers import AssetReadSerializer, AssetWriteSerializer
 from asset.models import Asset
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
@@ -16,7 +16,7 @@ class AssetView(ListCreateAPIView):
     pagination_class = LimitOffsetPagination
 
     def post(self, request, format=None):
-        serializer = AssetSerializer(data=request.data)
+        serializer = AssetWriteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -51,9 +51,9 @@ class AssetView(ListCreateAPIView):
 @receiver(pre_save, sender=Asset)
 def log_asset_changes(sender, instance, **kwargs):
     old_instance = Asset.objects.filter(pk=instance.pk).values().first()
-    if old_instance and instance.approval_status == "APPROVED":
+    if old_instance and instance.approval_status == "APPROVED" or instance.approval_status == "REJECTED":
         changes = {
-            field: (old_instance[field], getattr(instance, field))
+            field: (getattr(instance, field))
             for field in old_instance
             if field != "asset_uuid"
         }
