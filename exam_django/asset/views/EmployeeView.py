1e3805c1988cf2b022336ab7f2174bd6a5d1c5b7
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-02-28 06:55:49 +0530

    Feat retrieve employee details (#33)
    * feat: BusinessUnit
    Post
    
    * feat: Retrieve businessunit details
    
    * fix:Retrieve BusinessUnit
    
    * feat:Retrieve employee details
    
    * feat: Retrieve-Employee-Details
    
    * fix: Change bu to businessunit in BusinessUnitModel
    
    * fix:Employee url
    
    * fix: Add employee endpoint to url
    
    * fix:Add employee endpoint to urls
    
    * Update urls.py
    
    * feat:Add try exception block
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * fix: Add slash at end of urls endpoints
    
    * fix: Delete migration file
    
    ---------
    
    Co-authored-by: TC4Y <techarmy257@gmail.com>
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>

--- exam_django/asset/views/EmployeeView.py
+++ exam_django/asset/views/EmployeeView.py
@@ -7,14 +7,41 @@ from asset.models import Employee
 
 class EmployeeView(ListCreateAPIView):
     def post(self, request, format=None):
-        queryset = Employee.objects.all()
-        serializer = EmployeeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        data = {}
+        try:
+            queryset = Employee.objects.all()
+            serializer = EmployeeSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        "data": serializer.data,
+                        "message": "Employee successfully created",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            data["error"] = "Invalid request_status"
+            return Response(
+                {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     def get(self, request):
-        employee = Employee.objects.all()
-        serializer = EmployeeSerializer(employee, many=True)
-        return Response(serializer.data)
+        try:
+            employees = Employee.objects.all()
+            serializer = EmployeeSerializer(employees, many=True)
+            return Response(
+                {
+                    "data": serializer.data,
+                    "message": "Employee details successfully retrieved",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
