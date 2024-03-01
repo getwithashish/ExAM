bd99129d9e2fa955e62d0ab28a1c1e1891153796
Ashi941 <asimapal09@gmail.com>
2024-03-01 10:47:57 +0530

    fix: Added the id only for one

--- exam_django/asset/views/AssetLogView.py
+++ exam_django/asset/views/AssetLogView.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from asset.serializers import AssetLogSerializer
 import json
 from django.http import JsonResponse
-from asset.models import AssetLog, Location, BusinessUnit, AssetType, User, Memory
+from asset.models import AssetLog, Location, BusinessUnit, AssetType, User, Memory,Employee
 
 
 class AssetLogView(APIView):
@@ -36,7 +36,7 @@ class AssetLogView(APIView):
                         business_unit_obj.business_unit_name
                     )
 
-            #  Search asset type name if it exists
+            #  Search memory if it exists
             if "memory_id" in asset_log_json:
                 memory_id = asset_log_json.pop(
                     "memory_id"
@@ -45,23 +45,63 @@ class AssetLogView(APIView):
                 if memory_obj:
                     asset_log_json["memory_space"] = memory_obj.memory_space
 
-            #  Search memory name if it exists
-            if "location_id" in asset_log_json:
-                location_id = asset_log_json.pop(
-                    "location_id"
+            #  Search asset type name if it exists
+            if "asset_type_id" in asset_log_json:
+                asset_type_id = asset_log_json.pop(
+                    "asset_type_id"
                 )  # Remove location_id from the dictionary
-                location_obj = Location.objects.filter(id=location_id).first()
+                asset_type_obj = AssetType.objects.filter(id=asset_type_id).first()
                 if location_obj:
-                    asset_log_json["location"] = location_obj.location_name
+                    asset_log_json["asset_type"] = asset_type_obj.asset_type_name
 
-            #  #  Search location name if it exists
-            # if 'conceder_id' in asset_log_json:
-            #     conceder_id = asset_log_json.pop('conceder_id')  # Remove location_id from the dictionary
-            #     conceder_obj = User.objects.filter(id=id).first()
-            #     if conceder_obj:
-            #         asset_log_json['conceder'] = conceder_obj.first_name + conceder_obj.last_name
+             #  Search invoice location name if it exists
+            if "invoice_location_id" in asset_log_json:
+                invoice_location_id = asset_log_json.pop(
+                    "invoice_location_id"
+                )  # Remove location_invoice_id from the dictionary
+                location_obj = Location.objects.filter(id=invoice_location_id).first()
+                if location_obj:
+                    asset_log_json["invoice_location"] = location_obj.location_name
 
-            # Construct log data and append to response
+
+
+
+
+
+
+             #  Search conceder name if it exists
+            if 'conceder_id' in asset_log_json:
+                conceder_id = asset_log_json.pop('conceder_id')  # Remove location_id from the dictionary
+                conceder_obj = User.objects.filter(id=conceder_id).first()
+                conceder_object = {}
+                if conceder_obj:
+                    conceder_object = {
+                        "id": conceder_obj.id,
+                        "full_name": conceder_obj.first_name +" " +conceder_obj.last_name
+                    }
+                    
+                    
+                    asset_log_json['conceder'] = conceder_object
+            
+
+             #  Search custodian name if it exists
+            if 'custodian_id' in asset_log_json:
+                custodian_id = asset_log_json.pop('custodian_id')  # Remove location_id from the dictionary
+                custodian_obj = Employee.objects.filter(id=custodian_id).first()
+                if custodian_obj:
+                    asset_log_json['custodian name'] = custodian_obj.employee_name 
+
+
+             #  Search requester name if it exists
+            if 'requester_id' in asset_log_json:
+                requester_id = asset_log_json.pop('requester_id')  # Remove requester_id from the dictionary
+                requester_obj = User.objects.filter(id=requester_id).first()
+                if requester_obj:
+                    asset_log_json['rquester name'] = requester_obj.first_name +" " +requester_obj.last_name
+            
+           
+                    
+
             log_data = {
                 "id": log.id,
                 "timestamp": log.timestamp,
