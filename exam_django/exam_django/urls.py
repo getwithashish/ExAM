66865d2f9d198bcecf32af8ab29fa457f7ba8347
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-03-04 16:53:17 +0530

    Feat add asset drawer (#54)
    * feat: Add functionality to get the asset logs for a given asset_uuid (#42)
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * feat: Add functionality to get the asset logs for a given asset_uuid
    
    ---------
    
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>
    
    * feat:Add exception handling file (#44)
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * feat:Add exception handling file
    
    * feat: Add custom exceptions class
    
    ---------
    
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>
    
    * feat: Add Django authentication app
    
    * fix: Remove unwanted commands and imports
    
    * feat:add asset table component
    
    * feat: Add django authentication (#47)
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * feat: Add Django authentication app
    
    * fix: Remove unwanted commands and imports
    
    * Feat  asset log get (#46)
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * feat: Add functionality to get the asset logs for a given asset_uuid
    
    * feat: Add join query to location,business_unit and memory_space only
    
    * fix: Added the id only for one
    
    * feat: Add feature to display logs of an asset_id
    
    * fix: Flake8 errors were solved
    
    * fix: Add views.py in user_auth app
    
    * fix: Denote migrations folder of user_auth app as a package
    
    ---------
    
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>
    
    * add new user table for authentication (#49)
    
    * feat:add new user table for authentication
    
    * fix:fixed requester foreign key for new User table
    
    ---------
    
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>
    
    * fix:drawer component input field
    
    * feat:Add asset table drawer
    
    * feat:Update index.tsx
    
    ---------
    
    Co-authored-by: Asima Pal <91839451+Ashi941@users.noreply.github.com>
    Co-authored-by: getwithashish <ashishsamtgeorge@mca.ajce.in>
    Co-authored-by: Ashish Sam T George <77051732+getwithashish@users.noreply.github.com>
    Co-authored-by: AnanthanCJ <152945570+AnanthanCJ@users.noreply.github.com>
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>

--- exam_django/exam_django/urls.py
+++ exam_django/exam_django/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/asset/", include("asset.urls")),
+    path("api/v1/user/", include("user_auth.urls")),
 ]
--- exam_frontend/src/pages/index.tsx
+++ exam_frontend/src/pages/index.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Badge, Dropdown, Table, useTheme } from "flowbite-react";
+import { Badge, Dropdown, useTheme } from "flowbite-react";
 import type { FC } from "react";
 import Chart from "react-apexcharts";
 import NavbarSidebarLayout from "../layouts/navbar-sidebar";
 // import Addasset from '../components/Addasset'
 import AddAsset from "../components/AddAsset/AddAsset";
 
+// import { FaFilter } from "react-icons/fa";
+import { Drawer, Table } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
+import React, { useState, useEffect } from 'react';
+import AssetTable from '../components/AssetTable';
 
 const DashboardPage: FC = function () {
   return (
@@ -16,9 +21,9 @@ const DashboardPage: FC = function () {
         
         </div>
         {/* <Addasset/> */}
-        <AddAsset/>
+        {/* <AddAsset/> */}
       
-        
+        <AssetTable />
      
         <div className="my-6">
       
