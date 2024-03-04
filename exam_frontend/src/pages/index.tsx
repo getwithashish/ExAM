185bfc9d7977f0ae81104d1d953a442237ea3521
Aidrin Varghese <aidrinv2001@gmail.com>
2024-03-04 22:16:02 +0530

    Feat dashboard (#59)
    * add new user table for authentication (#49)
    
    * feat:add new user table for authentication
    
    * fix:fixed requester foreign key for new User table
    
    ---------
    
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>
    
    * feat: Allow only authenticated users to retrieve and create business units (#48)
    
    * fix: Resolve Asset Nested Serialization bug
    
    * fix: Add constraints to Asset model fields
    
    * fix: reuse asset model in asset log model
    
    * feat: Allow only authenticated users to retrieve and create business units
    
    * add permission classes (#55)
    
    * feat:add permission classes
    
    * feat:update requirements.txt
    
    * updated endpoint to get userlist
    
    ---------
    
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>
    
    * feat: Fixed issue with charts loading in Dashboard
    
    ---------
    
    Co-authored-by: AnanthanCJ <152945570+AnanthanCJ@users.noreply.github.com>
    Co-authored-by: AnanthanCJ <acj88177@gmail.com>
    Co-authored-by: Ashish Sam T George <77051732+getwithashish@users.noreply.github.com>

--- exam_frontend/src/pages/index.tsx
+++ exam_frontend/src/pages/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Badge, Dropdown, useTheme } from "flowbite-react";
 import type { FC } from "react";
-
 import NavbarSidebarLayout from "../layouts/navbar-sidebar";
 // import Addasset from '../components/Addasset'
 import AddAsset from "../components/AddAsset/AddAsset";
@@ -12,22 +11,22 @@ import type { TableColumnsType, TableProps } from 'antd';
 import React, { useState, useEffect } from 'react';
 import AssetTable from '../components/AssetTable';
 import { Assignment } from "../components/Assign/Assignment";
+import { Statistics } from "../components/charts/piechartBody";
 
 
 const DashboardPage: FC = function () {
   return (
     <NavbarSidebarLayout>
-      <div className="px-4 pt-6">
-    
-        <div className="my-6">
+      <div>
+  
+        <Statistics />        
         
-        </div>
         {/* <Addasset/> */}
-        {/* <AddAsset/> */}
+        <AddAsset/>
       
         <AssetTable />
      
-        <div className="my-6">
+        <div>
       
         </div>
       </div>
