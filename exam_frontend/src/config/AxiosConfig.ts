867973c3377b9120220dc31ef9fd65b27ec42708
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-03-05 22:54:55 +0530

    feat: Add axios config file

---
+++ exam_frontend/src/config/AxiosConfig.ts
@@ -0,0 +1,31 @@
+import axios, { InternalAxiosRequestConfig } from 'axios';
+ 
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api/v1',
+});
+
+const excludePaths = ['/user/register', '/user/signin'];
+ 
+axiosInstance.interceptors.request.use(
+
+  (config:InternalAxiosRequestConfig<any>) => {
+
+    const isExcludedPath = excludePaths.some((path) => config.url?.includes(path));
+
+    if (!isExcludedPath) {
+
+      const authToken = localStorage.getItem("jwt");
+      config.headers.Authorization = `Bearer ${authToken}`;
+    }
+ 
+    return config;
+
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+ 
+export  default axiosInstance;
