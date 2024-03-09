4f7f9775d72418cfb0b3a385c13ce3c787575a63
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-03-09 14:24:31 +0530

    Feat add search logic asset table (#91)
    * fix: Resolve bug in AssetTable
    
    * feat:add search logic to all the filter models
    
    * feat:add api responses to memory view.
    
    * fix:update page size in settings
    
    * fix:add api repsonse to location view

--- exam_django/exam_django/settings.py
+++ exam_django/exam_django/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [],
     "UNAUTHENTICATED_USER": None,
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 1,
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
