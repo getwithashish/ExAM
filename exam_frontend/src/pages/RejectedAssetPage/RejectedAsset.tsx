d2afc22871e44f8c93b0e457e227e6a66f87ca96
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-03-18 13:56:45 +0530

    feat:resolve bugs

--- exam_frontend/src/pages/RejectedAssetPage/RejectedAsset.tsx
+++ exam_frontend/src/pages/RejectedAssetPage/RejectedAsset.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import AssetTableHandler from '../../components/AssetTable/AssetTableHandler'
-import AssetTableHandler from '../../components/AssetTable/AssetTableHandler'
 
 const RejectedAsset = () => {
-  let queryParamProp = "&asset_detail_status=CREATE_REJECTED|UPDATE_REJECTED&assign_status=REJECTED"
-  let heading="Rejected Asset Details"
   let queryParamProp = "&asset_detail_status=CREATE_REJECTED|UPDATE_REJECTED&assign_status=REJECTED"
   let heading="Rejected Asset Details"
   return (
