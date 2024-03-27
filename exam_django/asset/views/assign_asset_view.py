98c70c0d942cb13c3e8d4ec3a0cedd1ef5bb9970
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-03-27 20:02:16 +0530

    fix:update assign asset service class

--- exam_django/asset/views/assign_asset_view.py
+++ exam_django/asset/views/assign_asset_view.py
@@ -1,10 +1,9 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from asset.models import Asset, Employee
 from asset.serializers import AssignAssetSerializer
 from response import APIResponse
-from messages import ASSET_NOT_FOUND, STATUS_EXPIRED_OR_DISPOSED
+from messages import ASSET_NOT_FOUND
 from asset.service.asset_assign_crud_service.assign_asset_service import (
     AssignAssetService,
 )
@@ -26,24 +25,9 @@ class AssignAssetView(APIView):
             employee_id = request.data.get("id")
             asset_uuid = request.data.get("asset_uuid")
 
-            try:
-                asset = Asset.objects.get(asset_uuid=asset_uuid)
-            except Asset.DoesNotExist:
-                return APIResponse(
-                    message=ASSET_NOT_FOUND,
-                    status=status.HTTP_404_NOT_FOUND,
-                )
-
-            # Check if the asset status is expired or disposed
-            if asset.status in ["EXPIRED", "DISPOSED"]:
-                return APIResponse(
-                    message=STATUS_EXPIRED_OR_DISPOSED,
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
             # Assign the asset using the appropriate service based on requester's role
             message = AssignAssetService.assign_asset(
-                role, asset, employee_id, requester
+                role, asset_uuid, employee_id, requester
             )
 
             return APIResponse(
@@ -55,3 +39,47 @@ class AssignAssetView(APIView):
                 message=serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+
+# from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.permissions import IsAuthenticated
+# from asset.models import Asset, Employee
+# from asset.serializers import AssignAssetSerializer
+# from response import APIResponse
+# from messages import ASSET_NOT_FOUND, STATUS_EXPIRED_OR_DISPOSED
+# from asset.service.asset_assign_crud_service.assign_asset_service import (
+#     AssignAssetService,
+# )
+
+
+# class AssignAssetView(APIView):
+#     def get_permissions(self):
+#         if self.request.method == "POST":
+#             return [IsAuthenticated()]
+#         else:
+#             return super().get_permissions()
+
+#     def post(self, request):
+#         serializer = AssignAssetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             requester = request.user
+#             role = requester.user_scope
+
+#             employee_id = request.data.get("id")
+#             asset_uuid = request.data.get("asset_uuid")
+
+#             # Assign the asset using the appropriate service based on requester's role
+#             message = AssignAssetService.assign_asset(
+#                 role, employee_id, asset_uuid,requester
+#             )
+
+#             return APIResponse(
+#                 message=message,
+#                 status=status.HTTP_201_CREATED,
+#             )
+#         else:
+#             return APIResponse(
+#                 message=serializer.errors,
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
