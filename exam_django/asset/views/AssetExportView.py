a224414e28db53c4c4b4e74578be5dfcc8bc2532
AnanthanCJ <acj88177@gmail.com>
2024-03-27 14:23:40 +0530

    fix:update export view

--- exam_django/asset/views/AssetExportView.py
+++ exam_django/asset/views/AssetExportView.py
@@ -5,6 +5,6 @@ from asset.service.export_service.export_service import ExportService
 
 class AssetExportView(APIView):
 
-    def get(self, request):
+    def get(self):
         response = ExportService.export_asset()
         return response
