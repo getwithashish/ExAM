3566c23bffa7331a357bc2c8e1ea163da9a227ed
AnanthanCJ <acj88177@gmail.com>
2024-04-09 11:50:51 +0530

    querybuilder styling

--- exam_frontend/src/pages/index.tsx
+++ exam_frontend/src/pages/index.tsx
@@ -7,28 +7,12 @@ import DrawerViewRequest from "./RequestPage/DrawerViewRequest";
 import { Button } from "flowbite-react";
 
 const DashboardPage: FC = function () {
-  const [visible, setVisible] = useState(false);
-
-  const showQueryBuilder = () => {
-    setVisible(true);
-  };
-
-  const closeQueryBuilder = () => {
-    setVisible(false);
-  };
- 
+  
   return (
     <div className="bg-white">
       <Statistics />
       <DasboardAssetHandler />
-      <Button onClick={showQueryBuilder} className="bg-blue-500 p-2 ml-10 h-50 w-50 font-display">QueryBuilder</Button>
-      <DrawerViewRequest
-        title="Assign"
-        onClose={closeQueryBuilder}
-        visible={visible}
-      >
-        <QueryBuilderComponent />
-      </DrawerViewRequest>
+      
     </div>
   );
 };
