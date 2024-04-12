835b0307bf415aab54f861403db690b32ff78f08
Ashi941 <asimapal09@gmail.com>
2024-04-12 10:45:55 +0530

    add: backend complete for import as a csv file

--- exam_django/asset/service/data_import_service/data_import_service.py
+++ exam_django/asset/service/data_import_service/data_import_service.py
@@ -1,63 +1,59 @@
+# asset/service/data_import_service.py
 
 import csv
+import io
+from datetime import datetime
+from django.forms import ValidationError
 from asset.models import Asset, AssetType, BusinessUnit, Employee, Location, Memory
-
 
 class AssetImportService:
     @staticmethod
     def parse_and_add_assets(file_content, user):
-        # Split the file content into lines and decode it as UTF-8
-        print(user)
         csv_reader = csv.DictReader(file_content.decode("utf-8").splitlines())
 
-        # Get existing asset IDs and serial numbers from the database
         existing_asset_ids = set(Asset.objects.values_list("asset_id", flat=True))
-        existing_serial_numbers = set(
-            Asset.objects.values_list("serial_number", flat=True)
-        )
+        existing_serial_numbers = set(Asset.objects.values_list("serial_number", flat=True))
 
-        # Initialize counters for added and skipped assets
         added_assets_count = 0
         skipped_assets_count = 0
+        missing_fields_assets = []
 
-        # Iterate over each row in the CSV file
+        new_assets = []  # List to hold new assets for CSV output
+
         for row in csv_reader:
-            # Check if the asset ID or serial number already exists in the database
-            if (
-                row["asset_id"] in existing_asset_ids
-                or row["serial_number"] in existing_serial_numbers
-            ):
-                # If duplicate, skip this row and increment the skipped count
+            asset_id = row.get("asset_id", "")
+            serial_number = row.get("serial_number", "")
+
+            if asset_id in existing_asset_ids or serial_number in existing_serial_numbers:
                 skipped_assets_count += 1
                 continue
 
-            asset_type, asset_type_created = AssetType.objects.get_or_create(
-                asset_type_name=row["asset_type"]
-            )
-            business_unit, business_unit_created = BusinessUnit.objects.get_or_create(
-                business_unit_name=row["business_unit"]
-            )
-            custodian = Employee.objects.filter(employee_name=row["custodian"]).first()
-            location, location_created = Location.objects.get_or_create(
-                location_name=row["location"]
-            )
-            invoice_location, invoice_location_created = Location.objects.get_or_create(
-                location_name=row["invoice_location"]
-            )
-            memory, memory_created = Memory.objects.get_or_create(
-                memory_space=row["memory"]
-            )
+            mandatory_fields = ["asset_category", "asset_id", "version", "asset_type", "product_name", "serial_number", "model_number", "owner", "purchase_date", "warranty_period", "invoice_location", "business_unit"]
+            if any(row.get(field, "").strip() == "" for field in mandatory_fields):
+                missing_fields_assets.append(row)
+                continue
 
-            # Create a new Asset object with the data from the CSV row
-            Asset.objects.create(
-                asset_id=row["asset_id"],
+            try:
+                purchase_date = datetime.strptime(row["date_of_purchase"], "%Y-%m-%d").date()
+            except ValueError:
+                raise ValidationError("Purchase date has an invalid format. It must be in YYYY-MM-DD format.")
+
+            asset_type, asset_type_created = AssetType.objects.get_or_create(asset_type_name=row["asset_type"])
+            business_unit, business_unit_created = BusinessUnit.objects.get_or_create(business_unit_name=row["business_unit"])
+            custodian = Employee.objects.filter(employee_name=row["custodian"]).first()
+            location, location_created = Location.objects.get_or_create(location_name=row["location"])
+            invoice_location, invoice_location_created = Location.objects.get_or_create(location_name=row["invoice_location"])
+            memory, memory_created = Memory.objects.get_or_create(memory_space=row["memory"])
+
+            asset = Asset(
+                asset_id=asset_id,
                 version=row["version"],
-                asset_category=row["asset_category"],
+                asset_category=row["category"],
                 product_name=row["product_name"],
                 model_number=row["model_number"],
-                serial_number=row["serial_number"],
+                serial_number=serial_number,
                 owner=row["owner"],
-                date_of_purchase=row["date_of_purchase"],
+                date_of_purchase=purchase_date,
                 status=row["status"],
                 warranty_period=row["warranty_period"],
                 os=row["os"],
@@ -74,8 +70,8 @@ class AssetImportService:
                 created_at=row["created_at"],
                 updated_at=row["updated_at"],
                 is_deleted=row["is_deleted"],
-                approved_by=user,  # Assign the User object obtained from the JWT payload
-                requester_id=user.id,  # Assign the User ID obtained from the JWT payload
+                approved_by=user,
+                requester_id=user.id,
                 asset_type_id=asset_type.id if asset_type else None,
                 business_unit_id=business_unit.id if business_unit else None,
                 custodian_id=custodian.id if custodian else None,
@@ -84,17 +80,20 @@ class AssetImportService:
                 memory_id=memory.id if memory else None,
             )
 
-            # Update the existing asset ID and serial number sets and increment the added count
-            existing_asset_ids.add(row["asset_id"])
-            existing_serial_numbers.add(row["serial_number"])
+            new_assets.append(asset)
+
+            existing_asset_ids.add(asset_id)
+            existing_serial_numbers.add(serial_number)
             added_assets_count += 1
 
+        Asset.objects.bulk_create(new_assets)  # Bulk create new assets
+
         return AssetImportService._prepare_import_summary(
-            added_assets_count, skipped_assets_count
+            added_assets_count, skipped_assets_count, missing_fields_assets
         )
 
     @staticmethod
-    def _prepare_import_summary(added_assets_count, skipped_assets_count):
+    def _prepare_import_summary(added_assets_count, skipped_assets_count, missing_fields_assets):
         if added_assets_count == 0:
             message = "No new data found. All files are duplicates."
         else:
@@ -103,9 +102,23 @@ class AssetImportService:
                 f"{skipped_assets_count} duplicate entries omitted."
             )
 
-        # Return the message as a dictionary
         return {
             "message": message,
             "added_assets_count": added_assets_count,
             "skipped_assets_count": skipped_assets_count,
+            "missing_fields_assets": missing_fields_assets,
         }
+
+    @staticmethod
+    def generate_missing_fields_csv(missing_fields_assets):
+        output = io.StringIO()
+        csv_writer = csv.writer(output)
+
+        if missing_fields_assets:
+            header_row = missing_fields_assets[0].keys()
+            csv_writer.writerow(header_row)
+
+            for asset in missing_fields_assets:
+                csv_writer.writerow(asset.values())
+
+        return output.getvalue()
--- exam_django/asset/views/data_import_view.py
+++ exam_django/asset/views/data_import_view.py
@@ -1,13 +1,15 @@
-from django.http import JsonResponse
+# asset/views.py
+
+import csv
+import io
+from django.http import JsonResponse, HttpResponse
 from rest_framework.views import APIView
 from rest_framework import status
-from asset.service.data_import_service.data_import_service import (
-    AssetImportService,
-)
+from asset.models.asset import Asset
+from asset.service.data_import_service.data_import_service import AssetImportService
 from user_auth.rbac import IsLead
 from response import APIResponse
 from messages import INVALID_CSV_FILE_TYPE, FILE_NOT_FOUND
-
 
 class DataImportView(APIView):
     permission_classes = (IsLead,)
@@ -15,7 +17,6 @@ class DataImportView(APIView):
     def post(self, request, format=None):
         try:
             user = request.user
-
             file = request.FILES.get("file")
             if not file:
                 return APIResponse(
@@ -24,17 +25,25 @@ class DataImportView(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
-            # Use the AssetImportService to process the file
             result = AssetImportService.parse_and_add_assets(file.read(), user)
 
-            # Include import summary in the response
-            response_data = {
-                "message": result["message"],
-                "added_assets_count": result["added_assets_count"],
-                "skipped_assets_count": result["skipped_assets_count"],
-            }
+            if result["missing_fields_assets"]:
+                missing_fields_csv = AssetImportService.generate_missing_fields_csv(result["missing_fields_assets"])
+                response = HttpResponse(missing_fields_csv, content_type='text/csv')
+                response['Content-Disposition'] = 'attachment; filename="missing_fields.csv"'
+                return response
+            else:
+                # Generate CSV file with consolidated assets
+                assets = Asset.objects.all()
+                csv_data = io.StringIO()
+                csv_writer = csv.writer(csv_data)
+                csv_writer.writerow([field.name for field in Asset._meta.fields])  # Write header
+                for asset in assets:
+                    csv_writer.writerow([getattr(asset, field.name) for field in Asset._meta.fields])  # Write data rows
 
-            return JsonResponse(response_data, status=status.HTTP_200_OK)
+                response = HttpResponse(csv_data.getvalue(), content_type='text/csv')
+                response['Content-Disposition'] = 'attachment; filename="consolidated_assets.csv"'
+                return response
 
         except UnicodeDecodeError:
             return APIResponse(
