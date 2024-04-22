5a05003e51f216e6cb1530f9d6605724e1fb9844
Ashi941 <asimapal09@gmail.com>
2024-04-22 14:33:27 +0530

    feat:Export working here

--- exam_django/asset/views/asset_export_view.py
+++ exam_django/asset/views/asset_export_view.py
@@ -1,10 +1,17 @@
-
+# views.py
+ 
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from asset.service.export_service.export_service import ExportService
-
-
+ 
 class AssetExportView(APIView):
-
     def get(self, request):
-        response = ExportService.export_asset()
-        return response
+        # Retrieve the export format from query parameters or default to 'csv'
+         
+        export_format = request.query_params.get("export_format")
+        # Ensure the ExportService is correctly called with the specified format
+        if export_format in ['csv', 'xlsx', 'pdf']:
+            response = ExportService.export_asset(export_format)  # Pass request to the export_asset method
+            return response
+        else:
+            return HttpResponse("Invalid export format specified", status=400)
