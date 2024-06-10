ef9e915fa6ae4e984e0189b752a998dfead4b7dc
AidrinVarghese <aidrinv2001@gmail.com>
2024-06-10 21:36:48 +0530

    fix: chart api

--- exam_frontend/src/components/charts/api/ChartApi.ts
+++ exam_frontend/src/components/charts/api/ChartApi.ts
@@ -3,25 +3,32 @@ import { AssetData } from '../types/ChartTypes';
 
 let axiosInstance: AxiosInstance | null = null;
 
-const initializeAxiosInstance = () => {
+const initializeAxiosInstance = async () => {
   if (!axiosInstance) {
-    return import('../../../config/AxiosConfig').then(({ default: instance }) => {
-      axiosInstance = instance;
-      return axiosInstance;
-    });
+    const { default: instance } = await import('../../../config/AxiosConfig');
+    axiosInstance = instance;
+    return axiosInstance;
   } else {
     return Promise.resolve(axiosInstance);
   }
 };
 
-export const fetchAssetData = (): Promise<AssetData> => {
-  return initializeAxiosInstance().then((instance) => {
-    return instance.get('/asset/asset_count').then((res: any) => res.data.data);
+export const fetchAssetData = async(): Promise<AssetData> => {
+  return initializeAxiosInstance().then(async (instance) => {
+    const res = await instance.get('/asset/asset_count');
+    if(res.status !== 200){
+      throw new Error("Asset Count fetch unsuccessful")
+    }
+    return res.data.data;
   });
-};
+};  
 
-export const fetchAssetTypeData = (): Promise<any> => { 
-  return initializeAxiosInstance().then((instance) => {
-    return instance.get('/asset/asset_type').then((res: any) => res.data.data);
+export const fetchAssetTypeData = async(): Promise<any> => { 
+  return initializeAxiosInstance().then(async (instance) => {
+    const res = await instance.get('/asset/asset_type');
+    if(res.status !== 200){
+      throw new Error("Asset Count fetch unsuccessful")
+    }
+    return res.data.data;
   });
 };
