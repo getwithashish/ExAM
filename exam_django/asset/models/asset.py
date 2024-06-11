f17a1962b37b58e4e28ed8feeed2b77f02e69692
AnanthanCJ <acj88177@gmail.com>
2024-06-11 16:59:39 +0530

    changed the asset model

--- exam_django/asset/models/asset.py
+++ exam_django/asset/models/asset.py
@@ -98,9 +98,9 @@ class Asset(models.Model):
     memory = models.ForeignKey("Memory", on_delete=models.CASCADE, null=True)
     storage = models.CharField(max_length=50, null=True, blank=False)
     configuration = models.CharField(max_length=255, null=True, blank=False)
-    accessories = models.CharField(max_length=50, null=True, blank=False)
+    accessories = models.TextField(max_length=255, null=True, blank=False)
     notes = models.TextField(null=True)
-    license_type=models.CharField(max_length=50,null=True,blank=False)
+    license_type = models.CharField(max_length=50, null=True, blank=False)
     approved_by = models.ForeignKey(
         User,
         related_name="%(app_label)s_%(class)s_conceder",
@@ -125,8 +125,7 @@ class Asset(models.Model):
     approval_status_message = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # TODO requester should be null=False, right?
-    # I think so too - Ananthan
+   
     requester = models.ForeignKey(
         User,
         related_name="%(app_label)s_%(class)s_requester",
