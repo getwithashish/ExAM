8c8efc5b0bb20c5b9a33a33a717d9119800a3c03
Ashish Sam T George <77051732+getwithashish@users.noreply.github.com>
2024-06-26 15:07:15 +0530

    feat: Add Health Check endpoints (#319)
    * fix: Only show assets which are not deleted
    
    * fix: Fix Pagination bug
    
    * build: Dockerize backend and frontend services
    
    * build: Add CORS settings to .env files
    
    * feat: Show IN SERVICE status in dashboard chart
    
    * fix: Remove duplicate toggleModel method
    
    * feat: Add Health Check Endpoints for the application and the external services
    
    * feat: Check health of app and external service within a period of time
    
    - Check App health every 1 minute
    - Check External Service health every 5 minutes
    - Send report to sentry if exception occurs
    
    * refactor: Indent settings.py
    
    * refactor: Remove test method from urls.py

--- exam_django/exam_django/settings.py
+++ exam_django/exam_django/settings.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 from ms_identity_web.configuration import AADConfig
 from ms_identity_web import IdentityWebPython
 
+import sentry_sdk
+
 from utils.decouple_config_util import DecoupleConfigUtil
 
 
@@ -49,7 +51,11 @@ CORS_ALLOWED_ORIGINS = config(
 #     list(default_headers) + ["Content-Type"] + get_all_cors_headers()
 # )
 
-CELERY_BROKER_URL = config("CELERY_BROKER_URL")
+# Redis Configuration
+REDIS_URL = config("CELERY_BROKER_URL")
+
+# Celery Configuration
+CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
@@ -57,7 +63,36 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "UTC"
 CELERY_HEALTH_CHECKS = True
 
-CELERY_BROKER_CONNECTION_MAX_RETRIES = 5
+CELERY_BEAT_SCHEDULE = {
+    "check-app-health-every-30-seconds": {
+        "task": "utils.health_check_tasks.check_application_health",
+        "schedule": 60,  # every 1 minute
+    },
+    "check-external-health-every-10-seconds": {
+        "task": "utils.health_check_tasks.check_external_service_health",
+        "schedule": 300,  # every 5 minutes
+    },
+}
+
+# Django Health Check Configuration
+HEALTH_CHECK = {
+    "SUBSETS": {
+        "app": ["DatabaseBackend", "DefaultFileStorageHealthCheck"],
+        "external": ["CeleryPingHealthCheck", "RedisHealthCheck"],
+    }
+}
+
+# Sentry Configuration
+sentry_sdk.init(
+    dsn=config("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 # Application definition
 INSTALLED_APPS = [
@@ -67,6 +102,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # REST framework
     "rest_framework",
     "rest_framework_simplejwt",
     "django_rest_passwordreset",
@@ -74,7 +110,15 @@ INSTALLED_APPS = [
     "user_auth",
     "asset",
     "ai",
+    # Swagger
     "drf_yasg",
+    # Django Health Check
+    "health_check",
+    "health_check.db",
+    "health_check.cache",
+    "health_check.storage",
+    "health_check.contrib.redis",
+    "health_check.contrib.celery",
 ]
 
 MIDDLEWARE = [
