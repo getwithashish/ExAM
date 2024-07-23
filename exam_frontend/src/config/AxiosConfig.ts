a8043e2591c7f95e47326d8a765dec787cb0578d
ANANTHANCJ <acj88178@gmail.com>
2024-07-23 17:30:55 +0530

    fix : revert time out settings of django and axios

--- exam_frontend/src/config/AxiosConfig.ts
+++ exam_frontend/src/config/AxiosConfig.ts
@@ -2,7 +2,6 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env["VITE_CONFIG_URL"],
-  timeout: 100000,
 });
 
 const excludePaths = ["/user/register", "/user/signin"];
