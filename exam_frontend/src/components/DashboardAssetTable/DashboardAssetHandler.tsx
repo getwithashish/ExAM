09eeff99b625e311ec41ae221c0e8875d707911b
AnanthanCJ <152945570+AnanthanCJ@users.noreply.github.com>
2024-08-21 10:26:02 +0530

    fix : remove version,cofiguration fields from frontend (#466)

--- exam_frontend/src/components/DashboardAssetTable/DashboardAssetHandler.tsx
+++ exam_frontend/src/components/DashboardAssetTable/DashboardAssetHandler.tsx
@@ -331,22 +331,22 @@ const DashboardAssetHandler = ({
       ),
     },
 
-    {
-      title: "Version",
-      dataIndex: "version",
-      responsive: ["md"],
-      width: 120,
-      sorter: true,
-      sortOrder: sortedColumn === "version" ? sortOrder : undefined,
-      onHeaderCell: () => ({
-        onClick: () => handleSort("version"),
-      }),
-      render: (text: string, record: any) => (
-        <div style={{ color: "#ffffff" }}>
-          {renderClickableColumn("Version", "version")(text, record)}
-        </div>
-      ),
-    },
+    // {
+    //   title: "Version",
+    //   dataIndex: "version",
+    //   responsive: ["md"],
+    //   width: 120,
+    //   sorter: true,
+    //   sortOrder: sortedColumn === "version" ? sortOrder : undefined,
+    //   onHeaderCell: () => ({
+    //     onClick: () => handleSort("version"),
+    //   }),
+    //   render: (text: string, record: any) => (
+    //     <div style={{ color: "#ffffff" }}>
+    //       {renderClickableColumn("Version", "version")(text, record)}
+    //     </div>
+    //   ),
+    // },
 
     {
       title: "Os",
