340c6f4416a78bd5ef88183e7567fe687ce6ac1c
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-11-06 15:25:05 +0530

    feat: Include SSO login and callback urls

--- exam_django/exam_django/settings.py
+++ exam_django/exam_django/settings.py
@@ -7,9 +7,11 @@ from ms_identity_web import IdentityWebPython
 from celery.schedules import crontab
 import sentry_sdk
 
+from exam_django.config import GlobalConfig
 from utils.decouple_config_util import DecoupleConfigUtil
 
 
+GLOBAL_CONFIG = GlobalConfig.get_configuration()
 config = DecoupleConfigUtil.get_env_config()
 
 AAD_CONFIG = AADConfig.parse_json(file_path="aad.config.json")
@@ -107,6 +109,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
     # REST framework
     "rest_framework",
     "rest_framework_simplejwt",
@@ -137,6 +140,16 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "ms_identity_web.django.middleware.MsalMiddleware",
 ]
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "social_core.backends.azuread.AzureADOAuth2",
+    "social_core.backends.microsoft.MicrosoftOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
+
+SOCIAL_CALLBACK_URL = "http://localhost:8000/api/v1/user/auth/sso/{}"
 
 ROOT_URLCONF = "exam_django.urls"
 
@@ -204,7 +217,6 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
-        
     }
 }
 
--- exam_django/user_auth/views.py
+++ exam_django/user_auth/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 import jwt
 import secrets
 import string
@@ -5,6 +6,10 @@ from django.shortcuts import redirect
 from rest_framework import generics, status
 from rest_framework.response import Response
 import sentry_sdk
+from user_auth.service.sso_service.sso_callback_service import SSOCallbackService
+from user_auth.service.sso_service.user_details_service.microsoft_sso_user_details_service import (
+    MicrosoftSSOUserDetailsService,
+)
 from user_auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
 from user_auth.serializers import (
@@ -13,6 +18,8 @@ from user_auth.serializers import (
 )
 from django.db.models import Q
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from social_django.utils import load_strategy, load_backend
+from social_core.exceptions import AuthTokenError
 from response import APIResponse
 from messages import (
     USERS_RETRIEVED_SUCCESSFULLY,
@@ -146,6 +153,54 @@ class SSOCreateRetrieveView(generics.GenericAPIView):
 
         redirect_url = f"http://localhost:5173/sso/flow?refresh_token={tokens['refresh']}&access_token={tokens['access']}"
         return redirect(redirect_url)
+
+
+class SSOLogin(generics.GenericAPIView):
+
+    def get(self, request, provider):
+        strategy = load_strategy(request)
+        backend = load_backend(
+            strategy,
+            provider,
+            redirect_uri=settings.SOCIAL_CALLBACK_URL.format(provider),
+        )
+        auth_url = backend.auth_url()
+        return Response(status=status.HTTP_302_FOUND, headers={"Location": auth_url})
+
+
+class SSOCallback(generics.GenericAPIView):
+    def get(self, request, provider):
+
+        sso_user_details_service = None
+        global_config = settings.GLOBAL_CONFIG
+
+        code = request.query_params.get("code")
+
+        # TODO IdPs like github do not have the option to set user_scope. So, such IdPs should be avoided.
+        # TODO Proceed after SSO is given
+
+        if not code:
+            return Response(
+                status=status.HTTP_302_FOUND,
+                headers={"Location": global_config["global"]["hostname"]},
+            )
+
+        if provider == "azuread-oauth2":
+            sso_user_details_service = MicrosoftSSOUserDetailsService()
+
+        try:
+            return SSOCallbackService.handle_sso_callback(
+                provider=provider,
+                request=request,
+                sso_user_details_service=sso_user_details_service,
+            )
+
+        except AuthTokenError as e:
+            sentry_sdk.capture_exception(e)
+            return Response(
+                status=status.HTTP_302_FOUND,
+                headers={"Location": global_config["global"]["hostname"]},
+            )
 
 
 def generate_random_password(length=12):
