a090858783ffdfe8b6d58f5ab7b1b0a47941dca5
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-12-14 15:51:53 +0530

    feat: create configuration file for notifications
    - merge the config.toml and notification_config.toml dynamically inside the application

--- exam_django/exam_django/config.py
+++ exam_django/exam_django/config.py
@@ -8,4 +8,11 @@ class GlobalConfig:
     def get_configuration() -> Dict:
         with open("config.toml", mode="rt", encoding="utf-8") as fp:
             config = tomlkit.load(fp)
-        return config
+
+        with open("notification_config.toml", mode="rt", encoding="utf-8") as fp:
+            email_config = tomlkit.load(fp)
+
+        merged_config = config.copy()
+        merged_config.update(email_config)
+
+        return merged_config
