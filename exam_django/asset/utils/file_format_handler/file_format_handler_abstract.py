6b99730680fc0033f3e0f8ab121e650630b48d76
getwithashish <ashishsamtgeorge@mca.ajce.in>
2025-01-13 17:15:27 +0530

    fix: specify the methods as abstract in file format handler

--- exam_django/asset/utils/file_format_handler/file_format_handler_abstract.py
+++ exam_django/asset/utils/file_format_handler/file_format_handler_abstract.py
@@ -1,16 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class FileFormatHandlerAbstract(ABC):
 
     @staticmethod
+    @abstractmethod
     def parse_to_csv(file_content):
         pass
 
     @staticmethod
+    @abstractmethod
     def generate_from_list(csv_list):
         pass
 
     @staticmethod
+    @abstractmethod
     def export(assets, expiry_dates, exclude_fields, foreign_fields):
         pass
