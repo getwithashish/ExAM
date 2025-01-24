c57f52f5bd8d6235479bb63437e0ecade98f711c
getwithashish <ashishsamtgeorge@mca.ajce.in>
2025-01-24 14:28:14 +0530

    chore: escape characters in REDIRECT_URL of script when updating config.toml in backend

--- application_setup_scripts/linux/update_backend_config_toml.sh
+++ application_setup_scripts/linux/update_backend_config_toml.sh
@@ -16,10 +16,10 @@ CONFIG_FILE_PATH="$BACKEND_DIR/$CONFIG_FILE_NAME"
 
 if [[ "$ENVIRONMENT" == "Production" ]]; then
     HOSTNAME="https://$SYSTEM_IP"
-    REDIRECT_URL="https://$SYSTEM_IP/sso/flow?refresh_token={}&access_token={}"
+    REDIRECT_URL="https://$SYSTEM_IP/sso/flow\?refresh_token={}\&access_token={}"
 elif [[ "$ENVIRONMENT" == "Development" ]]; then
     HOSTNAME="http://$SYSTEM_IP:5173"
-    REDIRECT_URL="http://$SYSTEM_IP:5173/sso/flow?refresh_token={}&access_token={}"
+    REDIRECT_URL="http://$SYSTEM_IP:5173/sso/flow\?refresh_token={}\&access_token={}"
 else
     echo "Invalid environment specified. Use 'Production' or 'Development'."
     exit 1
