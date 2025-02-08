3e7441598da4450e5d715394bb26c81abb5a187e
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2025-02-08 17:57:15 +0530

    doc: add documentation for asset type service class

--- exam_django/asset/service/asset_type_crud_service/asset_type_service.py
+++ exam_django/asset/service/asset_type_crud_service/asset_type_service.py
@@ -1,3 +1,7 @@
+from typing import Any, Tuple, Dict
+from rest_framework import status
+from rest_framework.request import Request
+
 from asset.models import AssetType
 from asset.serializers import AssetTypeSerializer
 from messages import (
@@ -6,29 +10,53 @@ from messages import (
     INVALID_ASSET_TYPE,
     VALID_ASSET_TYPE,
 )
-from rest_framework import status
 
 
 class AssetTypeService:
+    """
+    Service class for operations related to asset type
+    """
+
     @staticmethod
-    def create_asset_type(data):
+    def create_asset_type(data) -> Tuple[Dict[str, Any], str, int]:
+        """
+        Create a new asset type.
+
+        Args:
+            data: asset type data.
+
+        Returns:
+            Tuple[Dict[str, Any], str, int]: The serialized asset type data, message, and the HTTP status code.
+        """
+
         serializer = AssetTypeSerializer(data=data)
-        message_success = VALID_ASSET_TYPE
-        message_failure = INVALID_ASSET_TYPE
+        message_success: str = VALID_ASSET_TYPE
+        message_failure: str = INVALID_ASSET_TYPE
+
         if serializer.is_valid():
             asset_type = serializer.save()
-            serialized_asset_type = AssetTypeSerializer(
-                asset_type
-            ).data  # Serialize the object
+            serialized_asset_type = AssetTypeSerializer(asset_type).data
+            
             return serialized_asset_type, message_success, status.HTTP_201_CREATED
+
         return serializer.errors, message_failure, status.HTTP_404_NOT_FOUND
 
     @staticmethod
-    def retrieve_asset_types(request):
+    def retrieve_asset_types(request: Request) -> Tuple[Dict[str, Any], str, int]:
+        """
+        Retrieve a list of asset types, optionally filtering by a search query.
+
+        Args:
+            request (Request): The HTTP request containing parameters.
+
+        Returns:
+            Tuple[Dict[str, Any], str, int]: The serialized list of asset types, message, and the HTTP status code.
+        """
+
         try:
             queryset = AssetType.objects.all()
-            message_success = ASSET_TYPE_RETRIEVE_SUCCESS
-            message_failure = ASSET_TYPE_RETRIEVE_FAILURE
+            message_success: str = ASSET_TYPE_RETRIEVE_SUCCESS
+            message_failure: str = ASSET_TYPE_RETRIEVE_FAILURE
 
             search_query = request.GET.get("query", None)
 
