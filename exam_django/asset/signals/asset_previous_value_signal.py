f6b4b69988e8f5017447057e7e02b3381deff3e1
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2025-02-08 17:58:27 +0530

    doc: add documentation for asset previous value signal

--- exam_django/asset/signals/asset_previous_value_signal.py
+++ exam_django/asset/signals/asset_previous_value_signal.py
@@ -1,4 +1,10 @@
 from django.dispatch import Signal
 
 
+
 asset_previous_value_signal = Signal()
+"""
+Signal used to trigger saving the log to the database.
+When an asset request is approved, this signal is sent and is received by the receiver that saves the current asset data in the log.
+The signal is also sent when an asset is soft-deleted/restored.
+"""
